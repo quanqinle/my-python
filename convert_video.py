53dd9b12caddd7863d88a13f755199b0313dc6a4
权芹乐 <quanqinle@gmail.com>
2019-03-27 14:59:50 +0800

    fixbug:when subtitle has comma

--- convert_video.py
+++ convert_video.py
@@ -2,6 +2,7 @@
 
 #===============================================================================
 # 视频处理
+# 视频转换成mp4格式，并将字幕烧制到视频中
 #===============================================================================
 # 
 
@@ -10,16 +11,16 @@ import shutil
 import time
 
 #单集动画（1~10分钟）
-src_dir = 'D:\\tmp\\video\\source\\'
-des_dir = 'D:\\tmp\\video\\target\\'
-log_file = r'D:/tmp/video/convert.csv'
-commad_line = 'ffmpeg.exe -ss 00:00:02 -i "{0}" -vf subtitles="{1}" "{2}"'
-commad_line2 = 'ffmpeg.exe -ss 00:00:02 -i "{0}" "{1}"'
+src_dir = 'D:\\tmp\\video-source\\'
+des_dir = 'D:\\tmp\\video-target\\'
+subtitle_dir = 'D:\\tmp\\video-subtitle\\'
+log_file = r'D:/log.csv'
+commad_line = 'ffmpeg.exe -i "{0}" -vf subtitles=\"{1}\" "{2}"'
+commad_line2 = 'ffmpeg.exe -i "{0}" "{1}"'
+
 #commad_line= commad_line.decode("utf-8").encode("gbk")
 
 def convert_video(filename):
-    # for filename in os.listdir(src_dir):
-    
     froot, fext = os.path.splitext(filename)
     fext = fext.lower()
     
@@ -34,33 +35,37 @@ def convert_video(filename):
     # 跳过已存在文件
     des_file = des_dir + (froot + ".mp4")
     if os.path.exists(des_file):
-        print("跳过已存在, " + des_file)
+        to_log("跳过已存在, " + des_file)
         return
 
     # 字幕文件
-    subtitle_file = src_dir + (froot + ".en.srt")
+    subtitle_file = (froot + ".en.srt")
 
     src_file = src_dir + filename
 
-    print(des_file)
-    print(commad_line.format(src_file, subtitle_file, des_file))
+    """
+    Note:
+    I think ffmpeg has some problems when it uses the parameter '-vf subtitles ='.
+    File directory, single quote, comma, etc. are not allowed in this parameter.
+    """ 
+    os.chdir(subtitle_dir) # ffmpeg提取mkv内的字幕时，有bug！
+    
     if '.mp4' == fext:
         if os.path.exists(subtitle_file):
-            os.system(commad_line.format(src_file, subtitle_file, des_file))
+            run_cmd(src_file, subtitle_file, des_file)
             to_log("[mp4有字幕], " + filename)
         else:
             shutil.copy(src_file, des_file)
             to_log("[mp4无字幕], " + filename)
     elif '.mkv' == fext:
-        os.chdir(src_dir) # ffmpeg提取mkv内的字幕时，有bug！
-        subtitle_file = froot + ".en.srt"
         if not os.path.exists(subtitle_file):
+            # todo
             subtitle_file = filename
+        run_cmd(src_file, subtitle_file, des_file)
         to_log("[mkv视频], " + filename)
-        os.system(commad_line.format(src_file, subtitle_file, des_file))
     elif '.webm' == fext:
         if os.path.exists(subtitle_file):
-            os.system(commad_line.format(src_file, subtitle_file, des_file))
+            run_cmd(src_file, subtitle_file, des_file)
             to_log("[webm有字幕], " + filename)
         else:
             os.system(commad_line2.format(src_file, des_file))
@@ -68,6 +73,26 @@ def convert_video(filename):
     else:
         to_log('[未知文件], ' + filename)
 
+#===============================================
+# 执行系统指令
+# Note: 为了规避ffmpeg缺陷，重命名字幕文件。最后再改回文件名
+#===============================================
+def run_cmd(srcpath, subtitlefile, despath):
+    new_name = time.time()
+    os.rename(subtitlefile, new_name)
+    os.system(commad_line.format(srcpath, new_name, despath))
+    os.rename(new_name, subtitlefile)
+
+#===============================================
+# mkv视频是否含有字幕
+#===============================================
+def mkv_has_subtitle(mkv_file):
+    cmd = "ffmpeg -i \"{0}\" -c copy -map 0:s -f null - -v 0 -hide_banner && echo $? || echo $?"
+    code = os.system(cmd.format(mkv_file))
+    # TODO 为调通
+    if code == 0:
+        return True
+    return False
 
 def to_log(line):
     fname = log_file
@@ -76,7 +101,9 @@ def to_log(line):
         fout.write(line)
         fout.write('\n')
 
-
+#===============================================
+# 并发任务
+#===============================================
 from multiprocessing import Process, Queue, Pool
 def batch_tast(processes=10):
     """ 并发 """
